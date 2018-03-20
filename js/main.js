bb20ece9fb393151f2d872befbf087a29176e1eb
Jonathan Disla <jon.disla@gmail.com>
2018-03-20 19:36:54 -0400

    file transfer

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 
 
-    var catArray = ['Lancer', 'Subaru', 'Ferrari', 'BMW', 'Honda', 'Volkswagon', 'Mazda']
+var carArray = ['Lancer', 'Subaru', 'Ferrari', 'BMW', 'Honda', 'Volkswagon', 'Mazda'];
 
-    var newButtons;
+var newButtons;
 
-//     function addBrand(){
-//     for(var i = 0; i < catArray.length; i++){
-    
-//     newButtons = $('.card-body').prepend('<button id=buttons class="btn btn-primary">' + catArray[i])}
+function addBrand() {
+    for (var i = 0; i < carArray.length; i++) {
 
-//     $('#type').keyup(function(){
-//         var userInput = $('#type').val()
-//         console.log(userInput.toLowerCase())
+    newButtons = $('.card-body').prepend('<button id="buttons" class="btn btn-primary" style="margin-left:5px;margin-bottom:5px">' + carArray[i])}
 
-//     $('.btn-danger').click(function(){
-//         $('#buttons').append(userInput)
-//     })
+    $('#type').keyup(function() {
+        var userInput = $('#type').val()
+        console.log(userInput.toLowerCase())
 
-// })}
-    function getData(){
-    
+    $('.btn-danger').click(function() {
+        $('#buttons').append(userInput)
+    })
+
+})}
+
+function getData(){
+
     // var userInput = $('#searchtext').val()
-    var userInput = $('#searchtext').val()
+    var userInput = $('#searchtext').val();
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=lj5JtcnVU0ydCVDeb7Y7pj7m3ZyfY3k4";
 
@@ -32,14 +33,15 @@
         console.log(response);
 
         for (i in response.data){
-            $('#test').append("<img src='"+response.data[i].images.original.url+"'style='margin-left:56px;margin-top:30px;height:250px; width:450px; opacity:1'/>")
+            $('#results').append("<img src='"+response.data[i].images.original.url+"'style='margin-left:56px;margin-top:30px;height:250px; width:450px; opacity:1'/>");
         }
+        $('#btnSearch').on('click', '.checkbox', function(){//VERY IMPORTANT FOR HOMEWORK ADD BUTTONS TO PAGE
+          var toDoNumber = $(this).attr('data-to-do')
+        })
 
+        // carArray.push($('#buttons')).append(userInput).val()
+        // console.log(carArray)
     });
-    
 }
-    
-    // addBrand()
-
-
+addBrand()
 getData()
