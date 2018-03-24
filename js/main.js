843e5cb2bcf0ad1c0acf877d015a79bea5dc9160
Jonathan Disla <jon.disla@gmail.com>
2018-03-23 23:38:09 -0400

    Almost final

--- js/main.js
+++ js/main.js
@@ -1,69 +1,90 @@
-var carArray = ['Lancer', 'Subaru', 'Ferrari', 'BMW', 'Honda', 'Volkswagon', 'Mazda'];
+var carArray = ['MCLAREN', 'SUBARU', 'FERRARI', 'BMW', 'BUGATTI', 'JAGUAR MOTORS'];
+      var aprvdArray = [ ]
 
-var newButtons;
+      // displayMovieInfo function re-renders the HTML to display the appropriate content
+      function displayMovieInfo() {
 
-function addBrand() {
-    for (var i = 0; i < carArray.length; i++) {
+        var userInput = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=lj5JtcnVU0ydCVDeb7Y7pj7m3ZyfY3k4&limit=10&offset=0&rating=PG&lang=en";
+        
 
-    newButtons = $('#buttonMenu').prepend('<button data-index="'+ i +'" class="btn btn-primary gifbutton" style="margin-left:5px;margin-bottom:5px">' + carArray[i])}
+        // Creating an AJAX call for the specific userInput button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          console.log(response)
 
-    var words = $('#type').keypress(function() {
-        var userIn = $('#type').val()
-        console.log(userIn.toLowerCase())
+          // Creating a div to hold the userInput
+          var carDiv = $("<div class='userInput'>");
+            $("#carArray-view").empty()
 
-    $('.btn-danger').click(function() {
-        $('#buttons').append(userIn)
-    })
-    
-    
-})}
+            for (i in response.data){
+          // Retrieving the URL for the image
+          var imgURL = response.data[i].images.original.url;
 
+          // Creating an element to hold the image
+          var image = $("<img style= margin-left:56px;margin-top:30px;height:250px; width:450px; opacity:1>").attr("src", imgURL );
+            
+          // Appending the image
+          carDiv.append(image);
 
-$('#buttonMenu').on('click', '.gifbutton', function(){/////////////????????????????????????
-     console.log($(this).data('index'))
-});
-    
+          // Putting the entire userInput above the previous carArray
+          $("#carArray-view").append(carDiv);
+          console.log(carDiv)}
+        });
 
-newvar=['first', 'second']
-    $('#test').on('click', function(){
-        newvar='first'
-        if (newvar[1])
-        console.log(".,..")
+      }
 
-        // $('#buttons').each(function(){
-        //     var index = carArray
-        //     if (index === 'Mazda'){
-        //     console.log('perfect')
-        //     }else{          $('#test')
-        //         console.log('yess')
-        //     }
-        // })
-})
+      // Function for displaying userInput data
+      function renderButtons() {
 
-function getData(){
+        // Deleting the carArray prior to adding new carArray
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#gifButton").empty();
 
-    // var userInput = $('#searchtext').val()
+        // Looping through the array of carArray
+        for (var i = 0; i < carArray.length; i++) {
 
-    var userInput = $('#searchtext').val();
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=lj5JtcnVU0ydCVDeb7Y7pj7m3ZyfY3k4&limit=10&offset=0&rating=PG&lang=en";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        $('#results').empty()
-        for (i in response.data){
-            $('#results').append("<img src='"+response.data[i].images.original.url+"'style='margin-left:56px;margin-top:30px;height:250px; width:450px; opacity:1'/>");
+          // Then dynamicaly generating buttons for each userInput in the array
+          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adding a class of userInput-btn to our button
+          a.addClass("userInput-btn");
+          // Adding a data-attribute
+          a.attr("data-name", carArray[i]);
+          // Providing the initial button text
+          a.text(carArray[i]);
+          // Adding the button to the gifButton div
+          $("#gifButton").append(a);
         }
-        $('#btnSearch').on('click', '.checkbox', function(){//VERY IMPORTANT FOR HOMEWORK ADD BUTTONS TO PAGE
-          var toDoNumber = $(this).attr('data-to-do')
-        })
+      }
+      
+    
+    
 
-        // carArray.push($('#buttons')).append(userInput).val()
-        // console.log(carArray)
-    });
-}
-addBrand()
-getData()
+      // This function handles events where a userInput button is clicked
+      $("#btnSearch").on("click", function(event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var userInput = $("#searchtext").val().trim().toLowerCase();
+        
+        if(userInput === 'honda' || userInput === 'jeep'|| userInput === 'mitsubishi'|| userInput === 'hyundai'|| userInput === 'volkswagon'||userInput ===  'toyota'||userInput ===  'fiat'||userInput ===  'mazda'||userInput ===  'gmc'||userInput ===  'audi'||userInput ===  'cadillac'||userInput ===  'ford'||userInput ===  'lincoln'||userInput ===  'chevrolet'||userInput ===  'chevy'||userInput ===  'chrysler'||userInput ===  'buick'||userInput ===  'tesla'||userInput ===  'pontiac'||userInput ===  'dodge'||userInput ===  'acura'||userInput ===  'infiniti'||userInput ===  'lexus'||userInput ===  'isuzu'||userInput ===  'suzuki'||userInput ===  'nissan'||userInput ===  'cobra'||userInput ===  'hyundai'||userInput ===  'kia'||userInput ===  'renault'||userInput ===  'volvo'||userInput ===  'saab'||userInput ===  'lotus'||userInput ===  'mini'||userInput ===  'mini cooper'||userInput ===  'rolls royce'||userInput ===  'land rover'||userInput ===  'lamborghini'||userInput ===  'aston martin'){
+        console.log('Working...')
+          }else{
+            alert('Try again')
+          location.reload()
+          }
+
+        // Adding userInput from the textbox to our array
+        carArray.push(userInput.toUpperCase());
+
+        // Calling renderButtons which handles the processing of our userInput array
+        renderButtons();
+      });
+
+      // Adding a click event listener to all elements with a class of "userInput-btn"
+      $(document).on("click", ".userInput-btn", displayMovieInfo);
+
+      // Calling the renderButtons function to display the intial buttons
+      renderButtons();
